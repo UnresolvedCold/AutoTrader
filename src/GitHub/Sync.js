275a509cccc6a147f068c4e1fb4d1b7004d5eaf5
Shubham Kumar <unresolved.shubham@gmail.com>
2021-03-06 05:02:49 +0530

    shell silent

--- src/GitHub/Sync.js
+++ src/GitHub/Sync.js
@@ -7,11 +7,11 @@ const Sync = () => {
   return cron.schedule(
     `0 ${Config.gitSyncTime[1]} ${Config.gitSyncTime[0]} * * *`,
     async () => {
+      shell.config.silent = true;
       if (!shell.which("git")) {
         shell.echo("Git not found.");
       } else {
         var res = shell.exec("git pull origin TestGithub");
-        //shell.echo(res);
 
         if (!res.includes("Already up to date")) {
           let today = new Date().toLocaleDateString();
