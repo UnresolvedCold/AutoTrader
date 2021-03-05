60fa9ffcabafea17473c1708c34e5001fb91c62a
Shubham Kumar <unresolved.shubham@gmail.com>
2021-03-06 02:20:50 +0530

    Configured project to use env variables

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,10 @@
 module.exports = {
-  Name: "First Last", // change
-  Id: "AB1234", // change
-  Password: "Password", // change
-  Pin: "123456", // change
+  Name: process.env.AutoTradePuppet_Name || "First Name", // change
+  Id: process.env.AutoTradePuppet_Id || "AB1234", // change
+  Password: process.env.AutoTradePuppet_Password || "Password", // change
+  Pin: process.env.AutoTradePuppet_Pin || "123456", // change
 
-  startTime: [1, 34], // [hr, min]
+  startTime: [2, 20], // [hr, min]
   endTime: [16, 0], // [hr, min]
   marketInterval: (s, e) => {
     var ti = (e[0] * 60 + e[1] - s[0] * 60 - s[1]) * 60 * 1000;
