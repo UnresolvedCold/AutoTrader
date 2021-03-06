5982b3b9b4ab0e5e20e8acb1bde2ed929ea7d64b
Shubham Kumar <unresolved.shubham@gmail.com>
2021-03-06 06:08:47 +0530

    Market Timing

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ module.exports = {
   Password: process.env.AutoTradePuppet_Password || "Password", // change
   Pin: process.env.AutoTradePuppet_Pin || "123456", // change
 
-  startTime: [5, 28], // [hr, min]
+  startTime: [8, 55], // [hr, min]
   endTime: [16, 0], // [hr, min]
   marketInterval: (s, e) => {
     var ti = (e[0] * 60 + e[1] - s[0] * 60 - s[1]) * 60 * 1000;
