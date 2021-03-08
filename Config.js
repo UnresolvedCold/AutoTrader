4ddb8f5408c99fe2d14b28c7ef365bad2c3bb19e
Shubham Kumar <unresolved.shubham@gmail.com>
2021-03-08 08:29:49 +0530

    dd

--- Config.js
+++ Config.js
@@ -5,7 +5,7 @@ module.exports = {
   Pin: process.env.AutoTradePuppet_Pin || "123456", // change
 
   startTime: [8, 21], // [hr, min]
-  endTime: [4, 0], // [hr, min]
+  endTime: [16, 0], // [hr, min]
   marketInterval: (s, e) => {
     var ti = (e[0] * 60 + e[1] - s[0] * 60 - s[1]) * 60 * 1000;
     return ti > 0 ? ti : 100000;
